b9c09e6bbccaa287b9fc678468f47316c8887b28
CHOICORE <choijaehyeong93@gmail.com>
2023-08-23 19:45:24 +0900

    refactor: application properties

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/common/property/AuthenticationProperties.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/common/property/AuthenticationProperties.kt
@@ -6,10 +6,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "authentication")
 data class AuthenticationProperties(
     val loginAttemptsLimit: Int,
-    val errorMessage: ErrorMessage,
-)
-
-data class ErrorMessage(
-    val invalidCredentials: String,
-    val exceededLoginAttempts: String,
 )
