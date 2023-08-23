c0ab0438a03c78d5fd94b388f6ae5ea53cfa1bf4
CHOICORE <choijaehyeong93@gmail.com>
2023-08-23 19:46:30 +0900

    feat: add jwt configuration

---
+++ src/main/kotlin/me/choicore/demo/springsecurity/config/JwtConfiguration.kt
@@ -0,0 +1,32 @@
+package me.choicore.demo.springsecurity.config
+
+import com.nimbusds.jose.jwk.JWK
+import com.nimbusds.jose.jwk.JWKSet
+import com.nimbusds.jose.jwk.RSAKey.Builder
+import com.nimbusds.jose.jwk.source.ImmutableJWKSet
+import me.choicore.demo.springsecurity.authentication.common.property.RsaKeyProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.oauth2.jwt.JwtDecoder
+import org.springframework.security.oauth2.jwt.JwtEncoder
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder
+
+
+@Configuration
+class JwtConfiguration(
+    private val rsaKeyProperties: RsaKeyProperties,
+) {
+    @Bean
+    fun jwtDecoder(): JwtDecoder = NimbusJwtDecoder.withPublicKey(rsaKeyProperties.publicKey).build()
+
+    @Bean
+    fun jwtEncoder(): JwtEncoder {
+        val jwk: JWK = with(rsaKeyProperties) {
+            Builder(publicKey)
+                .privateKey(privateKey)
+                .build()
+        }
+        return NimbusJwtEncoder(ImmutableJWKSet(JWKSet(jwk)))
+    }
+}
--- src/main/kotlin/me/choicore/demo/springsecurity/config/SecurityConfiguration.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/config/SecurityConfiguration.kt
@@ -1,10 +1,10 @@
 package me.choicore.demo.springsecurity.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import me.choicore.demo.springsecurity.authentication.service.DefaultAuthenticationEntryPoint
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
@@ -16,25 +16,21 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration(
 
-    @Bean
-    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
-
-    @Bean
-    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
-        return authenticationConfiguration.getAuthenticationManager()
-    }
-
+    private val objectMapper: ObjectMapper,
+) {
     private companion object {
         private const val API_END_POINT_PREFIX = "/v1"
         private val PERMIT_WHITE_LIST = arrayOf(
             PathRequest.toH2Console(),
             AntPathRequestMatcher("${API_END_POINT_PREFIX}/account/sign-in"),
             AntPathRequestMatcher("${API_END_POINT_PREFIX}/account/sign-out"),
-            AntPathRequestMatcher("${API_END_POINT_PREFIX}/account/sign-up"),
         )
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 
     @Bean
     fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
@@ -46,7 +42,11 @@ class SecurityConfiguration {
                 it.requestMatchers(*PERMIT_WHITE_LIST).permitAll()
                 it.anyRequest().authenticated()
             }
-
+            .exceptionHandling {
+                it.authenticationEntryPoint(DefaultAuthenticationEntryPoint(objectMapper))
+            }
         return httpSecurity.build()
     }
 }
+
+
