4406ab6ae9098026a32570ef6cb1c4f7d00409f8
CHOICORE <choijaehyeong93@gmail.com>
2023-08-24 19:40:45 +0900

    style: add @Suppress("unused")

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/UnauthorizedException.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/UnauthorizedException.kt
@@ -2,17 +2,27 @@ package me.choicore.demo.springsecurity.authentication.exception
 
 
 sealed class UnauthorizedException : RuntimeException {
+    @Suppress("unused")
     constructor() : super()
     constructor(message: String) : super(message)
+
+    @Suppress("unused")
     constructor(message: String, cause: Throwable) : super(message, cause)
+
+    @Suppress("unused")
     constructor(cause: Throwable) : super(cause)
 }
 
-class InvalidCredentialsException : UnauthorizedException(ERROR_MESSAGE) {
+sealed class InvalidCredentialsException : UnauthorizedException(ERROR_MESSAGE) {
+
     private companion object {
         const val ERROR_MESSAGE: String = "아이디 또는 비밀번호가 잘못되었습니다."
     }
 }
+
+class InvalidPasswordException : InvalidCredentialsException()
+
+class UsernameNotFoundException : InvalidCredentialsException()
 
 class ExceededLoginAttemptsException : UnauthorizedException(ERROR_MESSAGE) {
     private companion object {
