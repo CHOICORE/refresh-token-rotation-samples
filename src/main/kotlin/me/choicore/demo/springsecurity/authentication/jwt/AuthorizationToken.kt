983cf08385072389ead87b3e0c56a4457ec833ab
CHOICORE <choijaehyeong93@gmail.com>
2023-08-25 00:12:14 +0900

    style: change class name

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/jwt/AuthorizationToken.kt
+++
@@ -1,9 +0,0 @@
-package me.choicore.demo.springsecurity.authentication.jwt
-
-sealed class AuthorizationToken {
-    data class Bearer(val token: String) : AuthorizationToken() {
-        override fun toString(): String {
-            return "Bearer $token"
-        }
-    }
-}
