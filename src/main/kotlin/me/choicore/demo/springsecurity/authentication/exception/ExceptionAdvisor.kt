12f961a2a509b961e80b4f6a749b4c95aca53bdf
CHOICORE <choijaehyeong93@gmail.com>
2023-08-25 00:17:56 +0900

    refactor: handle unauthorized exception

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/ExceptionAdvisor.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/ExceptionAdvisor.kt
@@ -10,30 +10,6 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 class ExceptionAdvisor {
     @ExceptionHandler(UnauthorizedException::class)
     fun handleUnauthorizedException(e: UnauthorizedException): ResponseEntity<Any> {
-        val errorCode = getUnauthorizedErrorCode(e)
-
-        return ResponseEntity(
-            mapOf(
-                "code" to mapOf(
-                    "errorCode" to errorCode.code,
-                    "errorMessage" to errorCode.errorMessage
-                ), "message" to e.message
-            ), UNAUTHORIZED
-        )
+        return ResponseEntity(getUnauthorizedErrorMessage(e), UNAUTHORIZED)
     }
-}
-
-internal fun getUnauthorizedErrorCode(e: UnauthorizedException): UnauthorizedErrorCode {
-    return when (e) {
-        is UsernameNotFoundException -> UnauthorizedErrorCode.USERNAME_NOT_FOUND
-        is InvalidPasswordException -> UnauthorizedErrorCode.INVALID_PASSWORD
-        is ExceededLoginAttemptsException -> UnauthorizedErrorCode.EXCEEDED_LOGIN_ATTEMPTS
-    }
-}
-
-internal enum class UnauthorizedErrorCode(val code: String, val errorMessage: String) {
-    USERNAME_NOT_FOUND("0001", "INVALID_CREDENTIALS"),
-    INVALID_PASSWORD("0002", "INVALID_CREDENTIALS"),
-    EXCEEDED_LOGIN_ATTEMPTS("0003", "EXCEEDED_LOGIN_ATTEMPTS"),
-
 }
--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/UnauthorizedException.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/exception/UnauthorizedException.kt
@@ -1,20 +1,17 @@
 package me.choicore.demo.springsecurity.authentication.exception
 
+import org.springframework.security.core.AuthenticationException
 
-sealed class UnauthorizedException : RuntimeException {
-    @Suppress("unused")
-    constructor() : super()
+
+sealed class UnauthorizedException : AuthenticationException {
+
     constructor(message: String) : super(message)
 
     @Suppress("unused")
     constructor(message: String, cause: Throwable) : super(message, cause)
-
-    @Suppress("unused")
-    constructor(cause: Throwable) : super(cause)
 }
 
 sealed class InvalidCredentialsException : UnauthorizedException(ERROR_MESSAGE) {
-
     private companion object {
         const val ERROR_MESSAGE: String = "아이디 또는 비밀번호가 잘못되었습니다."
     }
