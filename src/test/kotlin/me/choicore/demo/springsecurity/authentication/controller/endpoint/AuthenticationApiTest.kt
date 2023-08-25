47d24c3e7d05f3d6653606a2c313b0211b87e53b
CHOICORE <choijaehyeong93@gmail.com>
2023-08-25 19:43:14 +0900

    test: mock test

--- src/test/kotlin/me/choicore/demo/springsecurity/authentication/controller/endpoint/AuthenticationApiTest.kt
+++ src/test/kotlin/me/choicore/demo/springsecurity/authentication/controller/endpoint/AuthenticationApiTest.kt
@@ -1,4 +1,65 @@
 package me.choicore.demo.springsecurity.authentication.controller.endpoint
 
+import me.choicore.demo.springsecurity.authentication.jwt.JwtAuthenticationTokenProvider
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
+import java.time.Instant
 
-class AuthenticationApiTest
+@SpringBootTest
+@AutoConfigureMockMvc(printOnlyOnFailure = false)
+class AuthenticationApiTest(
+    @Autowired private val mockMvc: MockMvc,
+    @Autowired private val jwtAuthenticationTokenProvider: JwtAuthenticationTokenProvider,
+) {
+
+
+    @Test
+    fun t1() {
+        // given
+        val jwt = jwtAuthenticationTokenProvider.generateToken("choicore", 3600)
+        // expect
+        mockMvc.get("") {
+            header(HttpHeaders.AUTHORIZATION, "Bearer $jwt")
+        }.andExpect {
+            status { isOk() }
+            content {
+                contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                encoding(Charsets.UTF_8.displayName())
+                json(
+                    """
+                    {
+                      "code": 0,
+                      "message": "Hello, World!"
+                    }
+                """.trimIndent()
+                )
+            }
+        }
+    }
+
+
+    @Test
+    fun `expired token then invalid token exception`() {
+        // given
+        val jwt = jwtAuthenticationTokenProvider.generateToken("choicore", Instant.EPOCH.epochSecond + 1, Instant.EPOCH)
+        // expect
+        mockMvc.get("") {
+            header(HttpHeaders.AUTHORIZATION, "Bearer $jwt")
+        }.andExpect {
+            status { isUnauthorized() }
+            content {
+                contentTypeCompatibleWith(MediaType.APPLICATION_JSON_VALUE)
+                encoding(Charsets.UTF_8.displayName())
+                header {
+                    exists(HttpHeaders.WWW_AUTHENTICATE)
+                }
+            }
+        }
+    }
+}
