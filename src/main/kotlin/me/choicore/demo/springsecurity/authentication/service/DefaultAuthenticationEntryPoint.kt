27c2bd1eec27fead0c046acb90527543cfad8521
CHOICORE <choijaehyeong93@gmail.com>
2023-08-25 19:42:16 +0900

    refactor: unauthorized exception handler

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/service/DefaultAuthenticationEntryPoint.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/service/DefaultAuthenticationEntryPoint.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import me.choicore.demo.springsecurity.authentication.exception.getUnauthorizedErrorMessage
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.core.AuthenticationException
@@ -14,6 +15,7 @@ class DefaultAuthenticationEntryPoint(
 ) : AuthenticationEntryPoint {
     override fun commence(request: HttpServletRequest, response: HttpServletResponse, authException: AuthenticationException) {
         with(response) {
+            this.setHeader(HttpHeaders.WWW_AUTHENTICATE, authException.message)
             status = HttpStatus.UNAUTHORIZED.value()
             contentType = MediaType.APPLICATION_JSON_VALUE
             val errorMessage: Map<String, Any> = getUnauthorizedErrorMessage(authException)
