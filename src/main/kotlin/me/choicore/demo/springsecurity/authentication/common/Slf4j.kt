be8400896ba5354dceaf221fa2ebbe43343212c3
CHOICORE <choijaehyeong93@gmail.com>
2023-08-26 10:47:46 +0900

    feat: add slf4j logger

---
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/common/Slf4j.kt
@@ -0,0 +1,7 @@
+package me.choicore.demo.springsecurity.authentication.common
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+inline val <reified T> T.Slf4j: Logger
+    get() = LoggerFactory.getLogger(T::class.java)
