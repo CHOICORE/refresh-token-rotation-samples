91803081d6fe1ac38966a26a44d3b899a9bb06a5
CHOICORE <choijaehyeong93@gmail.com>
2023-08-26 10:49:46 +0900

    style: rename property in jwtproperies

--- src/main/kotlin/me/choicore/demo/springsecurity/authentication/common/properties/JwtProperties.kt
+++ src/main/kotlin/me/choicore/demo/springsecurity/authentication/common/properties/JwtProperties.kt
@@ -6,11 +6,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 @ConfigurationProperties(prefix = "jwt")
 data class JwtProperties(
     val issuer: String,
-    val expiration: Expiration,
+    val accessExpiredAt: Long,
+    val refreshExpiredAt: Long,
+    // val expiration: Expiration,
 ) {
-    data class Expiration(
-        val accessToken: Long,
-        val refreshToken: Long,
-    )
+//    data class Expiration(
+//        val accessToken: Long,
+//        val refreshToken: Long,
+//    )
 }
 
